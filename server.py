8bbd35446223f13783e1a2f379064a07a601fc37
LC <lrc3233@gmail.com>
2014-11-25 22:53:15 -0800

    added server to parse system dictionary, submitted words are vetted against dictionary

---
+++ server.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, session, render_template, g, redirect, url_for, flash
+import jinja2
+import os
+
+app = Flask(__name__)
+app.secret_key = '\xf5!\x07!qj\xa4\x08\xc6\xf8\n\x8a\x95m\xe2\x04g\xbb\x98|U\xa2f\x03'
+app.jinja_env.undefined = jinja2.StrictUndefined
+
+word_dict = {}
+
+@app.route("/")
+def index():
+  word_dict = parse_dict('/usr/share/dict/words')
+  return render_template("boggle.html")
+
+@app.route("/check_word", methods=["POST"])
+def check_word():
+  word = request.form.get("word").lower()
+  if word_dict.get(word) == True:
+    return "True"
+  return "False"
+
+def parse_dict(fp):
+  for line in open(fp):
+    line = line.strip()
+    word_dict[line] = True
+  return word_dict
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, port=port)
